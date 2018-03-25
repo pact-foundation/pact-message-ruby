cc682ad65732a157872a254ca87e40677bdad508
Beth Skurrie <beth@bethesque.com>
2018-03-26 09:14:00 +1100

    chore: update release.sh

--- script/release.sh
+++ script/release.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
-git reset HEAD CHANGELOG.md lib/pact/message/version.rb # avoid accidentally double incrementing when previous run fails
-git checkout -- lib/pact/message/version.rb # avoid accidentally double incrementing when previous run fails
-# git checkout -- CHANGELOG.md # avoid accidentally double incrementing when previous run fails
+# avoid accidentally double incrementing when previous run fails
+git reset HEAD CHANGELOG.md lib/pact/message/version.rb
+git checkout -- lib/pact/message/version.rb
+git checkout -- CHANGELOG.md
 bundle exec bump ${1:-minor} --no-commit
 bundle exec rake generate_changelog
 git add CHANGELOG.md lib/pact/message/version.rb
 git commit -m "chore(release): version $(ruby -r ./lib/pact/message/version.rb -e "puts Pact::Message::VERSION")"
-bundle exec rake tag_for_release
+# bundle exec rake tag_for_release # TODO move release to travis
 bundle exec rake release
