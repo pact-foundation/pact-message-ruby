c25120ab5d62aa9961341441a8d0695d710a6e32
Beth Skurrie <beth@bethesque.com>
2020-02-13 16:30:13 +1100

    chore: update release scripts to release from travis

--- script/release.sh
+++ script/release.sh
@@ -11,14 +11,16 @@ git checkout -- CHANGELOG.md
 docker_build_bundle_base
 
 script/release/bump-version.sh $1
-VERSION=$(gem_version)
-
-TAG=$VERSION script/release/generate-changelog.sh
+script/release/generate-changelog.sh
 
 git add CHANGELOG.md lib/pact/message/version.rb
 git commit -m "chore(release): version ${VERSION}
 
 [ci-skip]"
-# bundle exec rake tag_for_release # TODO move release to travis
-docker_build_bundle_base
-on_docker "bundle exec rake release"
+
+VERSION=$(gem_version)
+TAG="v${VERSION}"
+git tag -a "${TAG}" -m "Releasing version ${TAG}"
+git push origin "${TAG}"
+git push origin master
+echo "Releasing from https://hub.docker.com/repository/docker/pactfoundation/pact-message-ruby/builds"
--- script/release/generate-changelog.sh
+++ script/release/generate-changelog.sh
@@ -2,13 +2,5 @@
 
 set -e
 
-: "${TAG:?Please set the TAG environment variable}"
-
 source script/docker-functions
-
-docker run --rm -it \
-  -e TAG=${TAG} \
-  -v ${PWD}:/app \
-  ${IMAGE} \
-  sh -c "bundle exec rake generate_changelog"
-
+on_docker "bundle exec rake generate_changelog"
