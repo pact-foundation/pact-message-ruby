d7b4e8cab93adaad290017e78c4529d03580c472
David Milanese <milanese.david@gmail.com>
2021-01-22 15:13:07 +1030

    chore: refactor CLI tests. Add test variation

--- spec/features/cli_spec.rb
+++ spec/features/cli_spec.rb
@@ -1,27 +1,40 @@
 require 'fileutils'
+require 'English'
 
-RSpec.describe "the CLI" do
-  CLI_SPEC_PACT_FILE_PATH = "./tmp/foo-bar.json"
-
-  before do
-    FileUtils.rm_rf CLI_SPEC_PACT_FILE_PATH
-  end
+RSpec.describe 'the CLI' do
+  CLI_SPEC_PACT_FILE_PATH = './tmp/foo-bar.json'.freeze
 
   let(:json) { File.read('spec/fixtures/message-v1-format.json') }
+  let(:command) { 'bundle exec bin/pact-message help' }
 
-  it "creates a pact file with the given message" do
-    output = `bundle exec bin/pact-message update '#{json}' --consumer Foo --provider Bar --pact-dir ./tmp`
-    exit_status = $?
-    puts output if exit_status != 0
-    expect(exit_status).to eq 0
-    expect(File.exist?(CLI_SPEC_PACT_FILE_PATH)).to be true
+  RSpec.shared_examples 'terminate successully to create pact file' do
+    before do
+      FileUtils.rm_rf CLI_SPEC_PACT_FILE_PATH
+      `#{command}`
+    end
+
+    it { expect($CHILD_STATUS).to be_success }
+
+    it 'is expected to create a pact file' do
+      expect(File).to exist(CLI_SPEC_PACT_FILE_PATH)
+    end
   end
 
-  it "creates a pact file with a message from the standard input" do
-    output = `echo '#{json}' | bundle exec bin/pact-message update --consumer Foo --provider Bar --pact-dir ./tmp`
-    exit_status = $?
-    puts output if exit_status != 0
-    expect(exit_status).to eq 0
-    expect(File.exist?(CLI_SPEC_PACT_FILE_PATH)).to be true
+  context 'when providing a message' do
+    let(:command) { "bundle exec bin/pact-message update '#{json}' --consumer Foo --provider Bar --pact-dir ./tmp" }
+
+    include_examples 'terminate successully to create pact file'
+  end
+
+  context 'when piping the message from standard input' do
+    let(:command) { "echo '#{json}' | bundle exec bin/pact-message update --consumer Foo --provider Bar --pact-dir ./tmp" }
+
+    include_examples 'terminate successully to create pact file'
+  end
+
+  context 'when piping the message from standard input with a dash' do
+    let(:command) { "echo '#{json}' | bundle exec bin/pact-message update - --consumer Foo --provider Bar --pact-dir ./tmp" }
+
+    include_examples 'terminate successully to create pact file'
   end
 end
