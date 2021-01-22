5cbb6645f79813e38837258c04b953e98446b159
David Milanese <milanese.david@gmail.com>
2021-01-22 14:22:45 +1030

    feat: allow pact-message update to receive JSON via the standard input

--- lib/pact/message/cli.rb
+++ lib/pact/message/cli.rb
@@ -9,13 +9,23 @@ module Pact
       method_option :pact_dir, required: true, desc: "The Pact directory"
       method_option :pact_specification_version, required: false, default: "2.0.0", desc: "The Pact Specification version"
 
-      desc 'update MESSAGE_JSON', 'Update a pact with the given message, or create the pact if it does not exist. The MESSAGE_JSON may be in the legacy Ruby JSON format or the v2+ format.'
-      def update(message)
+      # Update a pact with the given message, or create the pact if it does not exist
+      desc 'update MESSAGE_JSON', "Update/create a pact. If MESSAGE_JSON is omitted or '-', it is read from stdin"
+      long_desc <<-MSG, wrapping: false
+        Update a pact with the given message, or create the pact if it does not exist.
+        The MESSAGE_JSON may be in the legacy Ruby JSON format or the v2+ format.
+        If MESSAGE_JSON is not provided or is '-', the content will be read from 
+        standard input.
+      MSG
+      def update(maybe_json = '-')
         require 'pact/message'
         require 'pact/message/consumer/write_pact'
+
+        message_json = JSON.parse(maybe_json == '-' ? $stdin.read : maybe_json)
+
         pact_specification_version = Pact::SpecificationVersion.new(options.pact_specification_version)
-        message = Pact::Message.from_hash(JSON.load(message), { pact_specification_version: pact_specification_version })
-        Pact::Message::Consumer::WritePact.call(message, options.pact_dir, options.consumer, options.provider, options.pact_specification_version, :update)
+        message_hash = Pact::Message.from_hash(message_json, { pact_specification_version: pact_specification_version })
+        Pact::Message::Consumer::WritePact.call(message_hash, options.pact_dir, options.consumer, options.provider, options.pact_specification_version, :update)
       end
 
       desc 'reify', "Take a JSON document with embedded pact matchers and return a concrete example"
