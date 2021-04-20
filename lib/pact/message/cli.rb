2bab66cf22cb9036917f5b715fd3d506d0717cd4
Beth Skurrie <beth@bethesque.com>
2021-04-20 13:47:01 +1000

    fix: JSON load message document so that the Ruby objects are created properly
    Fixes: https://github.com/pact-foundation/pact-ruby-standalone/issues/65

--- lib/pact/message/cli.rb
+++ lib/pact/message/cli.rb
@@ -14,17 +14,17 @@ module Pact
       long_desc <<-MSG, wrapping: false
         Update a pact with the given message, or create the pact if it does not exist.
         The MESSAGE_JSON may be in the legacy Ruby JSON format or the v2+ format.
-        If MESSAGE_JSON is not provided or is '-', the content will be read from 
+        If MESSAGE_JSON is not provided or is '-', the content will be read from
         standard input.
       MSG
       def update(maybe_json = '-')
         require 'pact/message'
         require 'pact/message/consumer/write_pact'
 
-        message_json = JSON.parse(maybe_json == '-' ? $stdin.read : maybe_json)
+        message_object = JSON.load(maybe_json == '-' ? $stdin.read : maybe_json)
 
         pact_specification_version = Pact::SpecificationVersion.new(options.pact_specification_version)
-        message_hash = Pact::Message.from_hash(message_json, { pact_specification_version: pact_specification_version })
+        message_hash = Pact::Message.from_hash(message_object, { pact_specification_version: pact_specification_version })
         Pact::Message::Consumer::WritePact.call(message_hash, options.pact_dir, options.consumer, options.provider, options.pact_specification_version, :update)
       end
 
